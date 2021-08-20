87f079de98a974b3fa662e7d0e29c8be04a01a68
Sun-Kimleng <lengleng44448888@gmail.com>
2021-08-20 12:52:55 +0700

    update

--- ConditionalStatement2.php
+++ ConditionalStatement2.php
@@ -1,7 +1,43 @@
+
 <!DOCTYPE html>
+<html>
+	<head>
+		<style>
+			.comDetails{
+				margin-left: 60px;
+			}
+		</style>
+	</head>
+<body>
+<?php
+?>
+
+
+</body>
+</html><!DOCTYPE html>
 <html>
 <body>
 <?php
+$computer = [
+  ['Name'=>'Asus', 'Country'=> 'China', 'Founder'=>'aaaa', 'budget'=>1460],
+  ['Name'=>'Dell', 'Country'=> 'USA', 'Founder'=>'bbbb', 'budget'=>2000],
+  ['Name'=>'Lenovo', 'Country'=> 'France', 'Founder'=> 'cccc', 'budget'=>1300],
+  ['Name'=>'MSI', 'Country'=> 'China', 'Founder'=> 'dddd', 'budget'=>1700]
+ ];
+	echo 'This is lower computer company\'s budget : '.'<br />';
+ foreach ( $computer as $computerDetails){
+	 if ($computerDetails['budget'] < 1500){
+			echo "<span class='comDetails'>".'- '.$computerDetails['Name'] . '<br />'.'</span>';
+		}
+	 }
+	echo 'This is upper computer company\'s budget : '.'<br />';
+ foreach ( $computer as $computerDetails){
+	 if ($computerDetails['budget'] > 1500){
+			echo "<span class='comDetails'>".'- '.$computerDetails['Name'] . '<br />'.'</span>';
+		}
+	 }
+	
+	 
 ?>
 
 

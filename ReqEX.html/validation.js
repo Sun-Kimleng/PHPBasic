7a5db8aafe0474d1881ce2b4c0aa57864f8eec80
Sun-Kimleng <lengleng44448888@gmail.com>
2021-09-01 13:49:30 +0700

    update

--- ReqEX.html/validation.js
+++ ReqEX.html/validation.js
@@ -1,1 +1,24 @@
-var reg = /[a-z]/
+//two way to create RegExp
+// var reg = /[a-z]/ig;
+
+// var reg2 = new RegExp(/[a-z]/.'i');
+const inputs = document.querySelectorAll('input');
+const patterns = {
+    telephone: /^[0-9]{9,11}$/
+};
+
+//validation function
+function validate(field, regex){
+    if(regex.test(field.value)){ //auto create class to any input elements 
+        field.className = 'valid';//this will show valid
+    }else{
+        field.className ='invalid';//this will show invalid
+    }
+}
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', (e) => {
+        // console.log(e.target.attribute.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value])
+    //e.target is keyboard events and patterns is an object that we created at the top
+    });
+});
